1bf23a95ceb672d2688e01283580ab78202def22
Vineet K <vineet.keshari@gmail.com>
2025-06-14 15:37:58 +0530

    Minor refactoring in timezone utils

--- src/util/timeutil.py
+++ src/util/timeutil.py
@@ -9,51 +9,52 @@ class TimezonePeriod:
   end_date:   date
   tz:         timezone
 
-TZ_PACIFIC_DAYLIGHT = timezone(-timedelta(hours = 8), name = 'PDT')
-TZ_BRITISH_SUMMER = timezone(timedelta(hours = 1), name = 'BST')
-TZ_INDIA_STANDARD = timezone(timedelta(hours = 5, minutes = 30), name = 'IST')
-TZ_CHINA_STANDARD = timezone(timedelta(hours = 8), name = 'CST')
+class TimezoneHistory:
+  _tz_pacific_daylight = timezone(-timedelta(hours = 8), name = 'PDT')
+  _tz_british_summer = timezone(timedelta(hours = 1), name = 'BST')
+  _tz_india_standard = timezone(timedelta(hours = 5, minutes = 30), name = 'IST')
+  _tz_china_standard = timezone(timedelta(hours = 8), name = 'CST')
 
-# This list must be sorted by date
-TZ_HISTORY = [
-    TimezonePeriod(start_date = date(2020, 1, 1),
-                    end_date = date(2023, 5, 24),
-                    tz = TZ_PACIFIC_DAYLIGHT),
-    TimezonePeriod(start_date = date(2023, 5, 24),
-                    end_date = date(2023, 12, 30),
-                    tz = TZ_BRITISH_SUMMER),
-    TimezonePeriod(start_date = date(2023, 12, 30),
-                    end_date = date(2024, 1, 21),
-                    tz = TZ_INDIA_STANDARD),
-    TimezonePeriod(start_date = date(2024, 1, 21),
-                    end_date = date(2024, 2, 19),
-                    tz = TZ_CHINA_STANDARD),
-    TimezonePeriod(start_date = date(2024, 2, 19),
-                    end_date = date(2024, 11, 3),
-                    tz = TZ_INDIA_STANDARD),
-    TimezonePeriod(start_date = date(2024, 11, 3),
-                    end_date = date(2025, 2, 12),
-                    tz = TZ_CHINA_STANDARD),
-    TimezonePeriod(start_date = date(2025, 2, 12),
-                    end_date = date(2025, 5, 31),
-                    tz = TZ_INDIA_STANDARD)]
-FIRST_DATE = TZ_HISTORY[0].start_date
+  # This list must be sorted by date
+  _tz_history = [
+      TimezonePeriod(start_date = date(2020, 1, 1),
+                      end_date = date(2023, 5, 24),
+                      tz = _tz_pacific_daylight),
+      TimezonePeriod(start_date = date(2023, 5, 24),
+                      end_date = date(2023, 12, 30),
+                      tz = _tz_british_summer),
+      TimezonePeriod(start_date = date(2023, 12, 30),
+                      end_date = date(2024, 1, 21),
+                      tz = _tz_india_standard),
+      TimezonePeriod(start_date = date(2024, 1, 21),
+                      end_date = date(2024, 2, 19),
+                      tz = _tz_china_standard),
+      TimezonePeriod(start_date = date(2024, 2, 19),
+                      end_date = date(2024, 11, 3),
+                      tz = _tz_india_standard),
+      TimezonePeriod(start_date = date(2024, 11, 3),
+                      end_date = date(2025, 2, 12),
+                      tz = _tz_china_standard),
+      TimezonePeriod(start_date = date(2025, 2, 12),
+                      end_date = date(2025, 5, 31),
+                      tz = _tz_india_standard)]
+  _first_date = _tz_history[0].start_date
 
-last_end_date = FIRST_DATE
-for tzh in TZ_HISTORY:
-  assert tzh.start_date >= last_end_date
-  assert tzh.end_date > tzh.start_date
-  last_end_date = tzh.end_date
-print("VALIDATED TZ_HISTORY")
+  last_end_date = _first_date
+  for tzh in _tz_history:
+    assert tzh.start_date >= last_end_date
+    assert tzh.end_date > tzh.start_date
+    last_end_date = tzh.end_date
+  print("VALIDATED TIMEZONE HISTORY")
 
 class TimezoneUtil:
 
   @classmethod
   def adjust_datetime_timezone(cls, dt):
-    for tzh in TZ_HISTORY:
+    for tzh in TimezoneHistory._tz_history:
       adjusted_dt = dt.astimezone(tzh.tz)
       adjusted_date = adjusted_dt.date()
-      if adjusted_date < FIRST_DATE:
+      if adjusted_date < TimezoneHistory._first_date:
         return None
       if adjusted_date <= tzh.end_date:
         return adjusted_dt
