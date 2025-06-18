d31ccb50ee7992f543807f9ae629151d5d6031e9
Vineet K <vineet.keshari@gmail.com>
2025-06-18 13:16:18 +0530

    Refactor common histogram setup from specialty histogram to base histogram

--- src/graph/histogram.py
+++ src/graph/histogram.py
@@ -35,6 +35,8 @@ class Histogram:
     self.start_date = start_date
     self.end_date = end_date
     self.period = period
+
+    self.fig, self.ax = plt.subplots(figsize = self._resolution)
   
   def get_xmin(self):
     return self._record_to_xmin[self.record_type]
@@ -63,6 +65,25 @@ class Histogram:
     elif self.period == par.AggregationPeriod.MONTHLY:
       return 3
   
+  def init_plot(self, title_text, ylim):
+    self.ax.set_title(title_text)
+    self.ax.set_xlabel(self.record_type)
+    self.ax.set_ylabel("No. of {}".format(common.GraphText.get_period_text(self.period)))
+
+    self.ax.set_xlim(self.get_xmin(), self.get_xmax())
+
+    hist_bins = self.get_hist_bins()
+    self.ax.set_xticks(hist_bins[ : : self._major_x_ticks_spacing])
+    self.ax.set_xticks(hist_bins, minor = True)
+    self.ax.grid(True, which = 'both', axis = 'x', alpha = 0.3)
+
+    self.ax.set_ylim(0, ylim)
+    yticks_major, yticks_minor = common.GraphTickSpacer.get_ticks(0, ylim)
+    self.ax.set_yticks(yticks_major)
+    self.ax.set_yticks(yticks_minor, minor = True)
+    self.ax.grid(True, which = 'minor', axis = 'y', alpha = 0.3)
+    self.ax.grid(True, which = 'major', axis = 'y', alpha = 0.5)
+  
 
   def show_average(self, value, gmtp, value_format, annotate = False):
     plt.axvline(x = value, ymax = gmtp.get_y_current(),
@@ -76,11 +97,11 @@ class Histogram:
     if annotate:
       gmtp.plot_annotation(s = ("p{p}: " + value_format).format(p = p, v = value), x = value)
   
-  def show_interval(self, ax, ylim, x_low, x_high):
-    ax.add_patch(Rectangle((x_low, 0), height = ylim, width = x_high - x_low,
+  def show_interval(self, ylim, x_low, x_high):
+    self.ax.add_patch(Rectangle((x_low, 0), height = ylim, width = x_high - x_low,
                             linewidth = 0, alpha = 0.2, fill = True))
 
-  def show_stats(self, ax, data_series, percentiles, ylim,
+  def show_stats(self, data_series, percentiles, ylim,
                   show_total_count = False, show_average = False, annotate_average = False,
                   show_percentiles = False, annotate_percentiles = False,
                   show_normal_stats = False, show_order_stats = False,
@@ -120,9 +141,9 @@ class Histogram:
                               annotate = annotate_percentiles)
     
     if show_intervals:
-      self.show_interval(ax, ylim, mid50_low, mid50_high)
-      self.show_interval(ax, ylim, mid80_low, mid80_high)
-      self.show_interval(ax, ylim, mid90_low, mid90_high)
+      self.show_interval(ylim, mid50_low, mid50_high)
+      self.show_interval(ylim, mid80_low, mid80_high)
+      self.show_interval(ylim, mid90_low, mid90_high)
     
     y_positioner = common.YPositioner(y_start = 1.00 - self._text_spacing_factor,
                                       y_spacing = self._text_spacing_factor)
@@ -163,15 +184,14 @@ class Histogram:
       for i in range(top_values_count):
         gmtp.plot_annotation(s = value_format.format(v = top_n_values[-(i + 1)]))
 
-  def show_or_save(self, fig, show = False, save_filename = None):
+  def show_or_save(self, show = False, save_filename = None):
+    self.fig.tight_layout()
     if show:
-      fig.tight_layout()
       plt.show()
     if save_filename:
       save_file = self._dio.get_graph_filepath(self._subfolder / save_filename)
-      fig.tight_layout()
       save_file.parent.mkdir(exist_ok = True, parents = True)
-      fig.savefig(save_file)
+      self.fig.savefig(save_file)
       print ("Graph written to: {}".format(save_file))
       plt.close()
 
@@ -185,44 +205,28 @@ class SingleSeriesHistogram(Histogram):
                 start_date, end_date, period):
     Histogram.__init__(self, record_type, record_units, record_aggregation_type,
                         start_date, end_date, period)
-    self.data = data
+    self.data_series = list(sorted(data.values()))
+    self.ylim = self.get_ylim(self.data_series)
 
     actual_start_date = max(min(data.keys()), start_date)
     self.start_date = timeutil.CalendarUtil.get_period_start_date(actual_start_date, period)
     actual_end_date = min(max(data.keys()), end_date)
     self.end_date = timeutil.CalendarUtil.get_next_period_start_date(actual_end_date, period)
-  
-  def plot(self, show = False, save = False):
-    fig, ax = plt.subplots(figsize = self._resolution)
-    title_text = common.GraphText.get_graph_title(self.record_type, self.record_units,
-                                                  self.start_date, self.end_date,
-                                                  self.record_aggregation_type, self.period)
-    ax.set_title(title_text)
-    ax.set_xlabel(self.record_type)
-    ax.set_ylabel("No. of {}".format(common.GraphText.get_period_text(self.period)))
-
-    data_series = list(sorted(self.data.values()))
-
-    ax.set_xlim(self.get_xmin(), self.get_xmax())
-
-    hist_bins = self.get_hist_bins()
-    ax.set_xticks(hist_bins[ : : self._major_x_ticks_spacing])
-    ax.set_xticks(hist_bins, minor = True)
-    ax.grid(True, which = 'both', axis = 'x', alpha = 0.3)
-
-    ylim = self.get_ylim(data_series)
-    ax.set_ylim(0, ylim)
-    yticks_major, yticks_minor = common.GraphTickSpacer.get_ticks(0, ylim)
-    ax.set_yticks(yticks_major)
-    ax.set_yticks(yticks_minor, minor = True)
-    ax.grid(True, which = 'minor', axis = 'y', alpha = 0.3)
-    ax.grid(True, which = 'major', axis = 'y', alpha = 0.5)
-
-    ax.hist(data_series, bins = self.get_bin_count(),
-            range = (self.get_xmin(), self.get_xmax()),
-            alpha = 0.8)
     
-    self.show_stats(ax, data_series, percentiles = self._percentiles, ylim = ylim,
+    self.init_plot()
+  
+  def init_plot(self):
+    title_text = common.GraphText.get_graph_title(self.record_type, self.record_units,
+                                                        self.start_date, self.end_date,
+                                                        self.record_aggregation_type, self.period)
+    Histogram.init_plot(self, title_text, self.ylim)
+
+  def plot(self, show = False, save = False):
+    self.ax.hist(self.data_series, bins = self.get_bin_count(),
+                  range = (self.get_xmin(), self.get_xmax()),
+                  alpha = 0.8)
+    
+    self.show_stats(self.data_series, percentiles = self._percentiles, ylim = self.ylim,
                     show_average = True, annotate_average = True,
                     show_percentiles = True, annotate_percentiles = True,
                     show_total_count = True, show_normal_stats = True, show_order_stats = True,
@@ -231,6 +235,6 @@ class SingleSeriesHistogram(Histogram):
       save_filename = "{}_{}_{}_{}.png".format(self.record_type, self.period.name,
                                                 self.start_date.strftime("%Y%m%d"),
                                                 self.end_date.strftime("%Y%m%d"))
-      self.show_or_save(fig, show = show, save_filename = save_filename)
+      self.show_or_save(show = show, save_filename = save_filename)
     else:
-      self.show_or_save(fig, show = show)
+      self.show_or_save(show = show)
